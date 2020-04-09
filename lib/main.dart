604675fd886c7f3c92aca8c4c600ab8b60a3d207
Doan Thien Chinh <thienchinhbtr@gmail.com>
2020-04-09 21:49:48 +0700

    add flutter login

---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+          constraints: BoxConstraints.expand(),
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
+                child: Container(
+                  width: 70.0,
+                  height: 70.0,
+                  padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.black26),
+                  child: FlutterLogo(),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+                child: Text(
+                  "Hello\nWelcome Login form",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 30),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+                child: TextField(
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                  decoration: InputDecoration(
+                      labelText: "UserName",
+                      labelStyle:
+                          TextStyle(color: Colors.black26, fontSize: 15)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+                child: Stack(
+                  alignment: AlignmentDirectional.centerEnd,
+                  children: <Widget>[
+                    TextField(
+                      obscureText: true,
+                      style: TextStyle(fontSize: 18, color: Colors.black),
+                      decoration: InputDecoration(
+                          labelText: "Password",
+                          labelStyle:
+                              TextStyle(color: Colors.black26, fontSize: 15)),
+                    ),
+                    Text(
+                      "SHOW",
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 56,
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(7)),
+                    ),
+                    color: Colors.blue,
+                    onPressed: onSignInClick,
+                    child: Text(
+                      "Sign In",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                child: Container(
+                  width: double.infinity,
+                  height: 130,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "New user? Sign Up",
+                        style: TextStyle(color: Colors.black26, fontSize: 15),
+                      ),
+                      Text(
+                        "FORGOT PASSWORD",
+                        style: TextStyle(color: Colors.black26, fontSize: 15),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onSignInClick() {}
+}
