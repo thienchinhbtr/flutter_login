a6131ae0ab5bb27b286d32537a0a1f4cbc7d2e5f
Doan Thien Chinh <thienchinhbtr@gmail.com>
2020-04-10 23:04:20 +0700

    'full'

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterlogin/home.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Login());
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -10,114 +21,121 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   bool _showPass = false;
+  TextEditingController _usernameController = new TextEditingController();
+  TextEditingController _passwordController = new TextEditingController();
+  var _usernameErr = "Tài khoản không hợp lệ";
+  var _passwordErr = "Mật khẩu không hợp lệ";
+  var _userInvalid = false;
+  var _passInvalid = false;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
-          constraints: BoxConstraints.expand(),
-          color: Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
-                child: Container(
-                  width: 70.0,
-                  height: 70.0,
-                  padding: EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle, color: Colors.black26),
-                  child: FlutterLogo(),
-                ),
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+        constraints: BoxConstraints.expand(),
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
+              child: Container(
+                width: 70.0,
+                height: 70.0,
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle, color: Colors.black26),
+                child: FlutterLogo(),
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
-                child: Text(
-                  "Hello\nWelcome Login form",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                      fontSize: 30),
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+              child: Text(
+                "Hello\nWelcome Login form",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 30),
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
-                child: TextField(
-                  style: TextStyle(fontSize: 18, color: Colors.black),
-                  decoration: InputDecoration(
-                      labelText: "UserName",
-                      labelStyle:
-                          TextStyle(color: Colors.black26, fontSize: 15)),
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+              child: TextField(
+                style: TextStyle(fontSize: 18, color: Colors.black),
+                controller: _usernameController,
+                decoration: InputDecoration(
+                    errorText: _userInvalid ? _usernameErr : null,
+                    labelText: "UserName",
+                    labelStyle: TextStyle(color: Colors.black26, fontSize: 15)),
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
-                child: Stack(
-                  alignment: AlignmentDirectional.centerEnd,
-                  children: <Widget>[
-                    TextField(
-                      obscureText: !_showPass,
-                      style: TextStyle(fontSize: 18, color: Colors.black),
-                      decoration: InputDecoration(
-                          labelText: "Password",
-                          labelStyle:
-                              TextStyle(color: Colors.black26, fontSize: 15)),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+              child: Stack(
+                alignment: AlignmentDirectional.centerEnd,
+                children: <Widget>[
+                  TextField(
+                    controller: _passwordController,
+                    obscureText: !_showPass,
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                    decoration: InputDecoration(
+                        errorText: _passInvalid ? _passwordErr : null,
+                        labelText: "Password",
+                        labelStyle:
+                            TextStyle(color: Colors.black26, fontSize: 15)),
+                  ),
+                  GestureDetector(
+                    onTap: OnToggleShowPass,
+                    child: Text(
+                      _showPass ? "SHOW" : "HIDE",
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
                     ),
-                    GestureDetector(
-                      onTap: OnToggleShowPass,
-                      child: Text(
-                        _showPass? "SHOW" : "HIDE",
-                        style: TextStyle(
-                            color: Colors.blue,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold),
-                      ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+              child: SizedBox(
+                width: double.infinity,
+                height: 56,
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(7)),
+                  ),
+                  color: Colors.blue,
+                  onPressed: onSignInClick,
+                  child: Text(
+                    "Sign In",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              child: Container(
+                width: double.infinity,
+                height: 130,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "New user? Sign Up",
+                      style: TextStyle(color: Colors.black26, fontSize: 15),
+                    ),
+                    Text(
+                      "FORGOT PASSWORD",
+                      style: TextStyle(color: Colors.black26, fontSize: 15),
                     )
                   ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 56,
-                  child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(7)),
-                    ),
-                    color: Colors.blue,
-                    onPressed: onSignInClick,
-                    child: Text(
-                      "Sign In",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                child: Container(
-                  width: double.infinity,
-                  height: 130,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "New user? Sign Up",
-                        style: TextStyle(color: Colors.black26, fontSize: 15),
-                      ),
-                      Text(
-                        "FORGOT PASSWORD",
-                        style: TextStyle(color: Colors.black26, fontSize: 15),
-                      )
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
@@ -129,5 +147,26 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  void onSignInClick() {}
+  void onSignInClick() {
+    setState(() {
+      if (_usernameController.text.length < 6 ||
+          !_usernameController.text.contains("@")) {
+        _userInvalid = true;
+      } else {
+        _userInvalid = false;
+      }
+      if (_passwordController.text.length < 6) {
+        _passInvalid = true;
+      } else {
+        _passInvalid = false;
+      }
+      if (_userInvalid == false && _passInvalid == false) {
+        Navigator.push(context, MaterialPageRoute(builder: GotoHome));
+      }
+    });
+  }
+
+  Widget GotoHome(BuildContext context) {
+    return HomePage();
+  }
 }
